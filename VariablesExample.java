9102eb640d9bd4fbfd72c22e44845e438e56026d
Bhagyashree <bhagyashree0242@gmail.com>
2025-12-09 16:57:31 +0530

    Create VariablesExample.java

---
+++ VariablesExample.java
@@ -0,0 +1,18 @@
+java
+public class VariablesExample {
+    public static void main(String[] args) {
+        // Declaring variables of different types
+        int age = 25;
+        double salary = 55000.50;
+        char grade = 'A';
+        boolean isEmployed = true;
+        String name = "Alice";
+
+        // Printing them out
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Employed: " + isEmployed);
+    }
+}
