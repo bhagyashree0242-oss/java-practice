1fb519437ce688f96e873fa57abaeaac20a51892
Bhagyashree <bhagyashree0242@gmail.com>
2025-12-10 17:36:39 +0530

    Create StringReverse.java

---
+++ DSA/StringReverse.java
@@ -0,0 +1,10 @@
+public class StringReverse{
+  public static void main(String[] args){
+    String str="Bhagyashree";
+    String rev="";
+    for(int i=str.length() -1;i>0;i--0{
+      rev +=str.charAt(i);
+    }
+    System.out.println("Reserved string:"+rev);
+  }
+}
